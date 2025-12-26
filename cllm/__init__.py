2943f9c27c25e7a9027136fb58f170702adc8b51
Yannick Andrade <yanick.jair.ta@gmail.com>
2025-12-26 10:04:12 +0000

    fixing deploy

--- cllm/__init__.py
+++ cllm/__init__.py
@@ -1,6 +1,18 @@
 from cllm.config.schemas import CLMConfig, CLMOutput
 from cllm.core.encoder import CLMEncoder
 
-from .__version__ import __version__
+from .__version__ import __description__, __title__, __version__
 
-__all__ = ["__version__", "CLMEncoder", "CLMConfig", "CLMOutput"]
+__all__ = [
+    "__version__",
+    "__title__",
+    "__description__",
+    "CLMEncoder",
+    "CLMConfig",
+    "CLMOutput",
+]
+
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        setattr(__locals[__name], "__module__", "cllm")
